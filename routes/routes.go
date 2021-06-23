19e7fe10efcfe4862e7e01a7f9817541ffca3c74
dikanp <andika.dikanp@gmail.com>
2021-06-23 16:38:09 +0700

    first init

---
+++ routes/routes.go
@@ -0,0 +1,14 @@
+package routes
+
+import (
+	"AlterraStore/controllers"
+
+	"github.com/labstack/echo"
+)
+
+
+func New() *echo.Echo {
+	e := echo.New()
+	e.DELETE("/carts/:cartId/products/:productId", controllers.DeleteCartControllers)
+	return e
+}
---
+++ user.go
@@ -0,0 +1,12 @@
+// package alt
+
+// import {
+	
+// }
+
+// type Customer struct {
+// 	ID uint
+// 	CreatedAt time.Time
+// 	UpdatedAt time.Time
+// 	DeletedAt
+// }
